ede3f08217522c50f4e53446e58977e27d2f4942
gonlelo <gonlelo04@gmail.com>
2024-01-19 12:20:30 +0100

    F1

---
+++ Java2/pilotoF1/src/GestionPilotosF1/gestionPilotosF1.java
@@ -0,0 +1,13 @@
+package GestionPilotosF1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+import pilotoF1.Piloto;
+
+public class gestionPilotosF1 {
+    private static ArrayList<Piloto> pilotos = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+}
