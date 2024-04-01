c6af9585722d367d8779de7eda7706abbc860a84
gonlelo <gonlelo04@gmail.com>
2024-04-02 01:46:42 +0200

    Cambio de nombre y Hello World bien hecho

--- Java2/ejsBásicos(AulaVirtual)/src/ejsBásicos/HelloWorld.java
+++ Java2/ejsBásicos(AulaVirtual)/src/ejsBásicos/HelloWorld.java
@@ -1,12 +1,8 @@
 package ejsBásicos;
 
-import java.util.Scanner;
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		public float pulgadas;
-		
 		
 		System.out.println("Hello World");
 	}
