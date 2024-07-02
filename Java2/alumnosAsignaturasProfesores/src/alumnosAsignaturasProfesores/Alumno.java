a0ad1293d3c84efa007a8f200a22a6dfcd5da82f
gonlelo <gonlelo04@gmail.com>
2024-07-02 17:10:33 +0200

    ole

---
+++ Java2/alumnosAsignaturasProfesores/src/alumnosAsignaturasProfesores/Alumno.java
@@ -0,0 +1,15 @@
+package alumnosAsignaturasProfesores;
+
+public class Alumno {
+	Asignatura asig1;
+	Asignatura asig2;
+	Asignatura asig3;
+	public Alumno(Asignatura asig1, Asignatura asig2, Asignatura asig3) {
+		super();
+		this.asig1 = asig1;
+		this.asig2 = asig2;
+		this.asig3 = asig3;
+	}
+	
+
+}
---
+++ Java2/alumnosAsignaturasProfesores/src/alumnosAsignaturasProfesores/Asignatura.java
@@ -0,0 +1,23 @@
+package alumnosAsignaturasProfesores;
+
+public class Asignatura {
+	int id;
+	int nota;
+	
+	public Asignatura (int id) {
+		
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	public int getNota() {
+		return nota;
+	}
+
+	public void setNota(int nota) {
+		this.nota = nota;
+	}
+}
